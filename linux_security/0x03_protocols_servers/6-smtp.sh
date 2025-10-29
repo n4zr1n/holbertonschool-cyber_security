d5b4647189bbc2ceb2335d797ce49b1b897c0e90
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-10-29 22:30:42 +0400

    Update 6-smtp.sh

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-val=$(awk -F= '/^\s*smtpd_tls_security_level/ {gsub(/ /,"",$2); print $2; exit}' /etc/postfix/main.cf 2>/dev/null); [[ "$val" == "encrypt" ]] && echo "STARTTLS configured: $val" || echo "STARTTLS not configured"
+val=$(awk -F= '/^\s*smtpd_tls_security_level/ {gsub(/ /,"",$2); print tolower($2); exit}' /etc/postfix/main.cf)
+[[ $val == encrypt ]] && echo "STARTTLS configured" || echo "STARTTLS not configured"
