689c46a4ac995c8a46e36ec22516eb00faa982c0
root <nazrin.pashayeva.std@bhos.edu.az>
2025-11-30 18:32:50 +0000

    commit

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,14 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# Use logs.txt by default if no input file is provided
 LOGFILE="${1:-logs.txt}"
 
-# Step 1: Identify attacker IP
-ATTACKER_IP=$(grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' "$LOGFILE" \
+# Step 1: find attacker IP
+ATTACKER_IP=$(grep -o '[0-9]\{1,3\}\(\.[0-9]\{1,3\}\)\{3\}' "$LOGFILE" \
     | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')
 
-# Step 2: Extract User-Agent strings for that IP
+# Step 2: extract User-Agent strings for this IP
 grep "$ATTACKER_IP" "$LOGFILE" \
-    | grep -oP '"[^"]+"\s*"[^"]+"\s*"[^"]+"\s*"[^"]+"' \
-    | awk -F'"' '{print $(NF-1)}' \
-    | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
+    | awk -F\" '{print $(NF-1)}' \
+    | sort | uniq -c | sort -nr | head -1 \
+    | awk '{print $2}'
