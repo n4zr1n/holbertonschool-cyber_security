f2461a9a201ad0d6c86591c679bc0a54a69f0b14
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-12-07 10:07:05 -0800

    Update 3-ips.sh

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,1 +1,18 @@
-#
+#!/bin/bash
+
+if [ -z $1 ]
+then
+    LOGFILE="auth.log"
+else
+    LOGFILE=$1
+fi
+
+if [ ! -f $LOGFILE ]
+then
+    echo "Log file not found"
+    exit 1
+fi
+
+# Count unique IPs of successful SSH logins
+tail -n 1000 $LOGFILE | grep "Accepted password" | awk '{print $11}' \
+    | sort | uniq | wc -l
