8210145c22ee9d6557b8f2be16add25d57c8e6e8
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-12-07 10:14:58 -0800

    Update 4-firewall.sh

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,1 +1,17 @@
-#
+#!/bin/bash
+
+if [ -z $1 ]
+then
+    LOGFILE="auth.log"
+else
+    LOGFILE=$1
+fi
+
+if [ ! -f $LOGFILE ]
+then
+    echo "Log file not found"
+    exit 1
+fi
+
+# Count all firewall additions
+tail -n 1000 $LOGFILE | grep -iE "iptables.*-A|ufw allow" | wc -l
