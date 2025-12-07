2d2ade751ede3f3726c92f3fbd65feeac0a3214f
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-12-07 10:09:38 -0800

    Update 3-ips.sh

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -14,8 +14,8 @@ then
 fi
 
 tail -n 1000 $LOGFILE \
-    | grep -E "Failed password|Invalid user|Accepted password" \
-    | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" \
+    | grep -E "Accepted" \
+    | awk '/from/{for(i=1;i<=NF;i++)if($i=="from")print $(i+1)}' \
     | sort \
     | uniq \
     | wc -l
