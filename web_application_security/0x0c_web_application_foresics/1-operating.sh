633f4faab650abdd1857e545613a4e247d6241bb
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-12-07 10:05:02 -0800

    Update 1-operating.sh

--- web_application_security/0x0c_web_application_foresics/1-operating.sh
+++ web_application_security/0x0c_web_application_foresics/1-operating.sh
@@ -1,1 +1,16 @@
-#
+#!/bin/bash
+
+if [ -z $1 ]
+then
+    LOGFILE="dmesg"
+else
+    LOGFILE=$1
+fi
+
+if [ ! -f $LOGFILE ]
+then
+    echo "Log file not found"
+    exit 1
+fi
+
+grep -m 1 "Linux version" $LOGFILE
