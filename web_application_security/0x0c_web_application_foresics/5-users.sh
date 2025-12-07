b9c1c76a1ad35805b36341b3ec9ee88cfbebe512
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-12-07 10:54:10 -0800

    Update 5-users.sh

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,1 +1,17 @@
-#
+#!/bin/bash
+
+LOGFILE=${1:-auth.log}
+
+if [ ! -f $LOGFILE ]; then
+    echo "Log file not found"
+    exit 1
+fi
+
+# Extract created users
+tail -n 1000 $LOGFILE \
+    | grep -i "useradd" \
+    | grep -o "name=[^, ]*" \
+    | cut -d= -f2 \
+    | sort \
+    | uniq \
+    | paste -sd ","
