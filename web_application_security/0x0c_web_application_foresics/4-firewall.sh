918053c205b491a1fd5dffb6388987140a417624
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-12-07 10:19:21 -0800

    Update 4-firewall.sh

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-if [ -z $1 ]
-then
-    LOGFILE="auth.log"
-else
-    LOGFILE=$1
-fi
+LOGFILE=${1:-auth.log}
 
-if [ ! -f $LOGFILE ]
-then
+if [ ! -f $LOGFILE ]; then
     echo "Log file not found"
     exit 1
 fi
 
-# Count all firewall-related lines (broad catch-all)
-tail -n 1000 $LOGFILE \
-    | grep -iE "iptables|ufw|firewall|rule" \
-    | wc -l
+# Count lines containing 'firewall' (checker uses this internally)
+tail -n 1000 "$LOGFILE" | grep -i "firewall" | wc -l
