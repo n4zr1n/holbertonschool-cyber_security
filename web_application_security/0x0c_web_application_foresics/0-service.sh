3aa8bd5f337ee7abfffa667b283edbc69adca671
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2025-12-07 10:04:04 -0800

    Update 0-service.sh

--- web_application_security/0x0c_web_application_foresics/0-service.sh
+++ web_application_security/0x0c_web_application_foresics/0-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# If no argument, default to auth.log
+# Default log file to auth.log if no argument provided
 if [ -z $1 ]
 then
     LOGFILE="auth.log"
@@ -8,16 +8,16 @@ else
     LOGFILE=$1
 fi
 
-# Check if file exists
+# Validate file exists
 if [ ! -f $LOGFILE ]
 then
     echo "Log file not found"
     exit 1
 fi
 
-# Extract service names (example: sshd[1234] -> sshd)
-grep -Eo '[a-zA-Z0-9_-]+\[[0-9]\+\]' $LOGFILE | \
-    sed 's/\[[0-9]\+\]//' | \
+# Extract service names like: sshd[1234]
+grep -Eo '[a-zA-Z0-9_-]+\[[0-9]+\]' $LOGFILE | \
+    cut -d'[' -f1 | \
     sort | \
     uniq -c | \
     sort -nr
