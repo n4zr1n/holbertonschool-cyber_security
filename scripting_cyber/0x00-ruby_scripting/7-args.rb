23c7e24b0a8d176066be63e6527ab3d6247bef92
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2026-01-18 20:44:35 +0400

    Update 7-args.rb

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -5,7 +5,10 @@ def print_arguments
     puts 'No arguments provided.'
   else
     puts 'Arguments:'
-    puts
-    ARGV.each { |arg| puts arg }
+    puts ''
+
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
+    end
   end
 end
