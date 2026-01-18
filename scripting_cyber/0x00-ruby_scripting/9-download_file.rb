d725fc455624b1e9ed96dc3cb38a744ecef1327f
Nazrin Pashayeva <nazrin.pashayeva.std@bhos.edu.az>
2026-01-18 20:52:20 +0400

    Create 9-download_file.rb

---
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'fileutils'
+require 'uri'
+
+# Check arguments
+if ARGV.length != 2
+  puts 'Usage: 9-downloadfile.rb URL LOCALFILE_PATH'
+  exit
+end
+
+url, local_path = ARGV
+
+begin
+  puts "Downloading file from #{url}…"
+
+  # Open the URL and read content
+  URI.open(url) do |remote_file|
+    # Ensure directory exists
+    FileUtils.mkdir_p(File.dirname(local_path))
+    # Write to local file
+    File.open(local_path, 'wb') do |file|
+      file.write(remote_file.read)
+    end
+  end
+
+  puts "File downloaded and saved to #{local_path}."
+rescue StandardError => e
+  puts "Error downloading file: #{e.message}"
+end
